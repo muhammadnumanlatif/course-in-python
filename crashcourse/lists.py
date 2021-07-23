eb6f5033156a54bb2a9c401c6f2eb670e50c3ea3
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2021-07-24 03:10:54 +0500

    lists

---
+++ crashcourse/lists.py
@@ -0,0 +1,13 @@
+
+shopping_list=[
+    "apples",
+    "meat",
+    "eggs",
+    "milk"
+]
+
+print(shopping_list)
+shopping_list.pop(1)
+print(shopping_list)
+shopping_list.append("bread")
+print(shopping_list)
